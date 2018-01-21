52b9501b583f5e8e0076c7fcdb91ae4253772526
David Alexander <opensource@thelonelyghost.com>
2018-01-21 16:37:36 -0500

    Modifies tests to check that CLI interface works

--- test/test_install.py
+++ test/test_install.py
@@ -1,5 +1,19 @@
 import sh
+from io import StringIO
+from tor_worker import __version__
 
 def test_is_installed():
     cmd_exists = sh.Command('command').bake('-v')
     assert cmd_exists('tor-worker').exit_code == 0
+
+def test_version():
+    buf = StringIO()
+    sh.tor_worker('--version', _out=buf)
+
+    assert 'tor-worker' in buf.getvalue()
+
+def test_subcommands():
+    with StringIO() as buf:
+        sh.tor_worker('work', '--help', _out=buf)
+        out = buf.getvalue()
+        assert 'tor-worker work' in out

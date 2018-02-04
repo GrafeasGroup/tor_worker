8fe03f21335f35ab1437fd42fa4f1fb57d630b60
David Alexander <opensource@thelonelyghost.com>
2018-02-04 02:09:07 -0500

    Pulls out cached_property decorator to central location

--- tor_worker/tasks/base.py
+++ tor_worker/tasks/base.py
@@ -7,52 +7,7 @@ import requests
 import redis
 from slackclient import SlackClient
 
-from tor_worker import __version__
-
-
-_missing = object()
-
-
-# @see https://stackoverflow.com/a/17487613/1236035
-class cached_property(object):  # pragma: no cover
-    """A decorator that converts a function into a lazy property.  The
-    function wrapped is called the first time to retrieve the result
-    and then that calculated result is used the next time you access
-    the value::
-
-        class Foo(object):
-
-            @cached_property
-            def foo(self):
-                # calculate something important here
-                return 42
-
-    The class has to have a `__dict__` in order for this property to
-    work.
-    """
-
-    # implementation detail: this property is implemented as non-data
-    # descriptor. non-data descriptors are only invoked if there is no
-    # entry with the same name in the instance's __dict__. this allows
-    # us to completely get rid of the access function call overhead. If
-    # one chooses to invoke __get__ by hand the property will still work
-    # as expected because the lookup logic is replicated in __get__ for
-    # manual invocation.
-
-    def __init__(self, func, name=None, doc=None):
-        self.__name__ = name or func.__name__
-        self.__module__ = func.__module__
-        self.__doc__ = doc or func.__doc__
-        self.func = func
-
-    def __get__(self, obj, _type=None):
-        if obj is None:
-            return self
-        value = obj.__dict__.get(self.__name__, _missing)
-        if value is _missing:
-            value = self.func(obj)
-            obj.__dict__[self.__name__] = value
-        return value
+from tor_worker import __version__, cached_property
 
 
 class InvalidUser(Exception):
@@ -73,9 +28,7 @@ class Task(celery.Task):
 
     @cached_property
     def redis(self):  # pragma: no cover
-        conn = redis.StrictRedis()
-
-        return conn
+        return redis.StrictRedis()
 
     @cached_property
     def http(self):  # pragma: no cover
@@ -88,8 +41,7 @@ class Task(celery.Task):
 
     @cached_property
     def slack(self):  # pragma: no cover
-        if os.getenv('SLACK_API_KEY'):
-            return SlackClient(os.getenv('SLACK_API_KEY'))
+        return SlackClient(os.environ['SLACK_API_KEY'])
 
     autoretry_for = ()
     retry_backoff = True

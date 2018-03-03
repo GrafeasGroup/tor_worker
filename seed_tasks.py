0daec8ac61846f911f84fbc894b0c0a7c32ac137
David Alexander <opensource@thelonelyghost.com>
2018-03-03 11:23:54 -0500

    Tests how signatures are handled if no task at given path

--- seed_tasks.py
+++ seed_tasks.py
@@ -2,18 +2,24 @@
 
 import time
 
+# We need this for routing:
 from tor_worker.app import app  # noqa
-from tor_worker.tasks.anyone import test_system  # noqa
-from tor_worker.tasks.moderator import send_bot_message  # noqa
 
+from celery import signature
 from redis import StrictRedis as RedisClient
 
 
 redis = RedisClient(host='localhost', port=6379, db=1)
 
+# Here are the tasks imported using an agnostic way of queuing them
+test_system = signature('tor_worker.tasks.anyone.test_system')  # noqa
+send_bot_message = signature('tor_worker.tasks.moderator.send_bot_message')  # noqa
+fake_task = signature('tor_worker.foo')  # noqa
+
 print('Pre-seed time:  %d' % time.time())
 
 for i in range(1, 500):
+    # fake_task.delay()
     test_system.delay()
     # send_bot_message.delay(f'test message {i}', to='spez',
     #                        subject=f'Test {i}')
@@ -26,7 +32,6 @@ while count > 0:
     print('Jobs left: %d' % count)
     time.sleep(1)
     count = redis.llen('celery')
-    pass
 
 stop = time.time()
 print('Done time:      %d' % stop)
